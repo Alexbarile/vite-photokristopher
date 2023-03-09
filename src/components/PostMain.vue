7a4fd77ff615d4fbfbd5cdfc69b2bb0b750f0a6d
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-03-09 17:03:10 +0100

    risolto problema di app.vue e scss. Fatto script di PostMain.vue

--- src/components/PostMain.vue
+++ src/components/PostMain.vue
@@ -7,28 +7,38 @@ export default{
         return{
             posts: [],
             loading: true,
-            baseUrl: 'http://127.0.0.1:8000/', 
+            baseUrl: 'http://127.0.0.1:8000/',
+            lastPage: null
         }
     },
     methods: {
-        getPosts(){
+        getPosts(post_page){
             this.loading = true;
-            axios.get(`${this.baseUrl}/api/posts`).then(response) => {
-                 if(response.data.success){
-                    this.posts = response.data.results 
-                 }
-            }
+            axios.get(`${this.baseUrl}/api/posts`, { params: { page: post_page }}).then((response) => {
+                if(response.data.success) {
+                    this.posts = response.data.posts.data
+                    this.currentPage = response.data.posts.current_page;
+                    this.lastPage = response.data.posts.last_page;
+                    this.loading = false
+                }
+                else {
+                    alert('Non completo')
+                }
+            });
         }
+    },
+    mounted() {
+        this.getPosts(this.currentPage);
     },
 }
 
 </script>
 
 <template>
-    <PostMain></PostMain>
+    ciao
 </template>
 
 <style lang="scss">
-@use './styles/app.scss' as *;
+@use '../styles/app.scss' as *;
 
 </style>
