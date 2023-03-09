e086b29e3ce727f4a971145e6b86a83b2b012b20
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-03-09 16:04:14 +0100

    inizio PostMain.vue

---
+++ src/components/PostMain.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios';
+
+export default{
+    name: "PostMain",
+    data(){
+        return{
+            posts: [],
+            loading: true,
+            baseUrl: 'http://127.0.0.1:8000/', 
+        }
+    }
+}
+
+</script>
+
+<template>
+    <PostMain></PostMain>
+</template>
+
+<style lang="scss">
+@use './styles/app.scss' as *;
+
+</style>
