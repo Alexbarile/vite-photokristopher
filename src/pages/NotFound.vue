1ddeb584aa9ce974f94b11ff2596f76064031feb
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-03-10 19:12:15 +0100

    component NotFound.vue creata

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,12 +1,19 @@
 <script>
 export default {
-    
+    name: 'NotFound'
 }
 </script>
 
 <template>
-    <div>
-        
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <h2>Pagina non trovata</h2>
+                <router-link :to="{name: 'post_link'}" class="btn btn-primary">
+                    Torna ai Post
+                </router-link>
+            </div>
+        </div>
     </div>
 </template>
 <style lang="scss">
