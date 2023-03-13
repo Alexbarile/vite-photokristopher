75d5bd462bf55ec42af7a124e0e292778256ce9a
Alexbarile <alexbarile93fg@yahoo.it>
2023-03-13 16:41:32 +0100

    fixato problema in SinglePost

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -56,41 +56,67 @@ export default {
                     <div class="col">
                         <div class="text-center d-flex flex-column">
                             <div>
-                                <strong>
-    
-                                </strong>
+                                <strong><i class="fas fa-map me-2">Indirizzo:</i></strong>
+                                Via, Numero, Città, CAP
                             </div>
-
                             <div>
-                                <strong>
-    
-                                </strong>
+                                <strong><i class="fas fa-book me-2">Partita Iva:</i></strong>
+                                123456789
                             </div>
-
                         </div>
                     </div>
                     
                     <div class="col">
-                        <div class="text-center d-flex flex-column">
+                        <div class="text-center">
                             <div>
-                                <strong>
-    
-                                </strong>
+                                <strong><i class="fas fa-phone me-2">Telefono:</i></strong>
+                                0123456789
                             </div>
-
                             <div>
-                                <strong>
-    
-                                </strong>
+                                <strong><i class="fas fa-envelope me-2">Email:</i></strong>
+                                info@boolean.it
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col m-5">
+                <h4 class="text-center">Scrivici</h4>
+                <form @submit.prevent="sendForm">
+                    <div class="row">
+                        <div class="col m-3">
+                            <label for="nome" class="control-label">Nome</label>
+                            <input type="text" class="form-control" :class="{'is-invalid' : errors.name}" name="nome" id="nome" placeholder="nome" v-model="name">
+                            <div v-for="{error, index} in errors.name" :key="`message-error-${index}`" class="text-white">
+                                {{ error }}
+                            </div>
+                        </div>
+                        <div class="col m-3">
+                            <label for="cognome" class="control-label">Cognome</label>
+                            <input type="text" class="form-control" name="cognome" id="cognome" placeholder="cognome" v-model="surname">
+                        </div>
+                        <div class="col m-3">
+                            <label for="email" class="control-label">Email</label>
+                            <input type="text" class="form-control" name="email" id="email" placeholder="email" v-model="email">
+                        </div>
+                        <div class="col m-3">
+                            <label for="telefono" class="control-label">Telefono</label>
+                            <input type="text" class="form-control" name="telefono" id="telefono" placeholder="telefono" v-model="phone">
+                        </div>
+                        <div class="col m-3">
+                            <button type="submit" class="send-email">
+                                Invia
+                            </button>
 
                         </div>
                     </div>
+                </form>
 
-                </div>
             </div>
         </div>
-        
+    </div>
+    <div class="container-fluid">
+        <h3 class="text-center">GOOGLE MAPS</h3>
     </div>
 </template>
 
--- src/pages/SinglePost.vue
+++ src/pages/SinglePost.vue
@@ -11,8 +11,8 @@ export default {
             loading: store.loading,
         }
     },
-    beforeMount() {
-        this.loading = true;
+    mounted() {
+        // this.loading = true;
         axios.get(`${this.baseUrl}/api/posts/${this.$route.params.slug}`).then((response) => {
             if (response.data.success) {
                 this.post = response.data.post
@@ -30,47 +30,47 @@ export default {
 <template>
     <div class="container">
         <div class="row">
-            <div class="col d-flex justify-content-center align-items-center align" v-if="this.loading">
+            <div class="col d-flex justify-content-center align-items-center align" v-if="loading">
                 <div class="lds-dual-ring"></div>
             </div>
             <div v-else class="col">
                 <div class="d-flex justify-content-end m-4">
                     <router-link class="btn btn-sm btn-success" :to="{ name: 'post_list'}">Torna ai Post</router-link>
                 </div>
-            </div>
-            <div class="card mt-4">
-                <div class="card-img-top">
-                    <div class="cover-img">
-                        <img :src="post.cover_image ? `${store.baseUrl}/storage/${post.cover_image}` : 'https://picsum.photos/200/300'" class="img-fluid" alt="">
+                <div class="card mt-4">
+                    <div class="card-img-top">
+                        <div class="cover-img">
+                            <img :src="post && post.cover_image ? `${store.baseUrl}/storage/${post.cover_image}` : 'https://picsum.photos/200/300'" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="card-title p-1">
+                    <h5>{{ post.title }}</h5>
+                    <div class="card-body">
+    
+                        <p v-if="post.type">
+                            <em v-if="post.type">
+                                <strong>Categoria</strong>: {{post.type.name}}
+                            </em>
+                            <em v-else>
+                                Nessuna Categoria
+                            </em>
+                        </p>
+                        <p v-if="post.technologies.length > 0">
+                            <em>
+                                <strong>Technologies</strong>:
+                                <span class="badge bg-primary m-2" v-for="technology in post.technologies" :key="technology.id">
+                                    {{technology.name}}
+                                </span>
+                            </em>
+                        </p>
+                        <p>
+                            <em>
+                                <strong>Descrizione</strong>: {{ post.content }}
+                            </em>
+                        </p>
                     </div>
                 </div>
-                <div class="card-title p-1">
-                <h5>{{ post.title }}</h5>
-                <div class="card-body">
-
-                    <p v-if="post.type">
-                        <em v-if="post.type">
-                            <strong>Categoria</strong>: {{post.type.name}}
-                        </em>
-                        <em v-else>
-                            Nessuna Categoria
-                        </em>
-                    </p>
-                    <p v-if="post.technologies.length > 0">
-                        <em>
-                            <strong>Technologies</strong>:
-                            <span class="badge bg-primary m-2" v-for="technology in post.technologies" :key="technology.id">
-                                {{technology.name}}
-                            </span>
-                        </em>
-                    </p>
-                    <p>
-                        <em>
-                            <strong>Descrizione</strong>: {{ post.content }}
-                        </em>
-                    </p>
                 </div>
-            </div>
             </div>
         </div>
     </div>
