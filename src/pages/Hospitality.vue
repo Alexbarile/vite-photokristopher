e2141cba429caf2a8e3903b04ebc1a8b8f353544
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-12-04 18:06:32 +0100

    update pages photos

--- src/pages/Hospitality.vue
+++ src/pages/Hospitality.vue
@@ -1,11 +1,6 @@
 <script>
 export default {
-    name: 'Hospitality',
-    data() {
-        return {
-    
-        };
-    },
+    name: 'Hospitality'
 }
 </script>
 
@@ -14,15 +9,6 @@ export default {
         <div class="row">
             <div class="col">
                 <div class="grid-wrapper">
-
-                    
-                   
-
-
-
-
-
-
                     <div class="square">
                         <img src="../../public/img/bars/DSCF6264-2.JPG" alt="" />
                     </div>
@@ -39,24 +25,21 @@ export default {
                         <img src="../../public/img/bars/DSCF8163.jpg" alt="" />
                     </div>
                     <div class="tall">
-                        <img src="../../public/img/bars/DSCF8174.jpg" alt="" />
-                    </div>
-                    <div class="big">
                         <img src="../../public/img/bars/DSCF8178.jpg" alt="" />
                     </div>
-                    <div class="square">
+                    <div class="big">
                         <img src="../../public/img/bars/DSCF8427.JPG" alt="" />
                     </div>
-                    <div class="wide">
+                    <div class="square">
                         <img src="../../public/img/bars/DSCF8452.JPG" alt="" />
                     </div>
-                    <div class="big">
+                    <div class="wide">
                         <img src="../../public/img/bars/DSCF8457.JPG" alt="" />
                     </div>
-                    <div class="tall">
+                    <div class="big">
                         <img src="../../public/img/bars/DSCF9211-3.jpg" alt="" />
                     </div>
-                    <div class="square">
+                    <div class="tall">
                         <img src="../../public/img/bars/DSCF9215.JPG" alt="" />
                     </div>
                     <div class="square">
@@ -66,49 +49,31 @@ export default {
                         <img src="../../public/img/bars/DSCF9240.JPG" alt="" />
                     </div>
                     <div class="square">
-                        <img src="../../public/img/bars/" alt="" />
-                    </div>
-                    <div class="wide">
-                        <img src="https://images.unsplash.com/photo-1588263823647-ce3546d42bfe?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=675&amp;q=80" alt="" />
+                        <img src="../../public/img/bars/FUJIFILM_X-T30_4039x6059_052936.jpg" alt="" />
                     </div>
                     <div class="square">
-                        <img src="https://images.unsplash.com/photo-1587732608058-5ccfedd3ea63?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" />
-                    </div>
-                    <div class="square">
-                        <img src="https://images.unsplash.com/photo-1587897773780-fe72528d5081?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1489&amp;q=80" alt="" />
-                    </div>
-                    <div class="wide">
-                        <img src="https://images.unsplash.com/photo-1588083949404-c4f1ed1323b3?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1489&amp;q=80" alt="" />
-                    </div>
-                    <div class="square">
-                        <img src="https://images.unsplash.com/photo-1587572236558-a3751c6d42c0?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" />
-                    </div>
-                    <div class="wide">
-                        <img src="https://images.unsplash.com/photo-1583542225715-473a32c9b0ef?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" />
+                        <img src="../../public/img/bars/FUJIFILM_X-T30_4066x6099_052930.jpg" alt="" />
                     </div>
                     <div class="big">
-                        <img src="https://images.unsplash.com/photo-1527928159272-7d012024eb74?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" />
+                        <img src="../../public/img/bars/FUJIFILM_X-T30_4105x6220_052927.jpg" alt="" />
                     </div>
                     <div class="square">
-                        <img src="https://images.unsplash.com/photo-1553984840-b8cbc34f5215?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" />
+                        <img src="../../public/img/bars/FUJIFILM_X-T30_4105x6220_054288.JPG" alt="" />
                     </div>
                     <div class="square">
-                        <img src="https://images.unsplash.com/photo-1433446787703-42d5bf446876?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1351&amp;q=80" alt="" />
-                    </div>
-                    <div class="big">
-                        <img src="https://images.unsplash.com/photo-1541187714594-731deadcd16a?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" alt="" />
-                    </div>
-                    <div class="tall">
-                        <img src="https://images.unsplash.com/photo-1540979388789-6cee28a1cdc9?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80" alt="" />
-                    </div>
-                    <div class="square">
-                        <img src="https://images.unsplash.com/photo-1421930866250-aa0594cea05c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1355&amp;q=80" alt="" />
-                    </div>
-                    <div class="square">
-                        <img src="https://images.unsplash.com/photo-1493306454986-c8877a09cbeb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1381&amp;q=80" alt="" />
+                        <img src="../../public/img/bars/FUJIFILM_X-T30_4116x6174_052923.jpg" alt="" />
                     </div>
                     <div class="wide">
-                        <img src="https://images.unsplash.com/photo-1536466528142-f752ae7bdd0c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" />
+                        <img src="../../public/img/bars/FUJIFILM_X-T30_4160x6240_054260.jpg" alt="" />
+                    </div>
+                    <div class="square">
+                        <img src="../../public/img/bars/FUJIFILM_X-T30_4160x6240_058109.jpg" alt="" />
+                    </div>
+                    <div class="wide">
+                        <img src="../../public/img/bars/FUJIFILM_X-T30_6240x4160_058087.jpg" alt="" />
+                    </div>
+                    <div class="big">
+                        <img src="../../public/img/bars/FUJIFILM_X-T30_6240x4160_058099.jpg" alt="" />
                     </div>
                 </div>
             </div>
@@ -165,7 +130,7 @@ img {
 .grid-wrapper .tall:hover, .big:hover, .wide:hover, .square:hover {
     // filter: grayscale(100%);
     scale: 2;
-    box-shadow: 0 0 6px 3px rgb(215, 215, 123);
+    box-shadow: 0 0 6px 3px rgb(239, 239, 211);
 }
 
     
--- src/pages/People.vue
+++ src/pages/People.vue
@@ -8,7 +8,59 @@ export default {
     <div class="container">
         <div class="row">
             <div class="col">
-                
+                <div class="grid-wrapper">
+                    <div class="square">
+                        <img src="../../public/img/people/DSCF1667-4.JPG" alt="" />
+                    </div>
+                    <div class="square">
+                        <img src="../../public/img/people/DSCF1712-2.JPG" alt="" />
+                    </div>
+                    <div class="tall">
+                        <img src="../../public/img/people/DSCF6374.JPG" alt="">
+                    </div>
+                    <div class="wide">
+                        <img src="../../public/img/people/DSCF6404.JPG" alt="" />
+                    </div>
+                    <div class="square">
+                        <img src="../../public/img/people/DSCF6411.JPG" alt="" />
+                    </div>
+                    <div class="tall">
+                        <img src="../../public/img/people/DSCF6454.JPG" alt="" />
+                    </div>
+                    <div class="big">
+                        <img src="../../public/img/people/DSCF6455.JPG" alt="" />
+                    </div>
+                    <div class="square">
+                        <img src="../../public/img/people/DSCF6492-2.JPG" alt="" />
+                    </div>
+                    <div class="wide">
+                        <img src="../../public/img/people/DSCF7420.JPG" alt="" />
+                    </div>
+                    <div class="big">
+                        <img src="../../public/img/people/DSCF7573-3.JPG" alt="" />
+                    </div>
+                    <div class="tall">
+                        <img src="../../public/img/people/DSCF9154.JPG" alt="" />
+                    </div>
+                    <div class="square">
+                        <img src="../../public/img/people/DSCF9269.JPG" alt="" />
+                    </div>
+                    <div class="square">
+                        <img src="../../public/img/people/DSCF9832-2.JPG" alt="" />
+                    </div>
+                    <div class="square">
+                        <img src="../../public/img/people/DSCfggF1643-12.JPG" alt="" />
+                    </div>
+                    <div class="square">
+                        <img src="../../public/img/people/FUJIFILM_X-T30_4160x6240_057864.JPG" alt="" />
+                    </div>
+                    <div class="big">
+                        <img src="../../public/img/people/FUJIFILM_X-T30_4160x6240_057895.JPG" alt="" />
+                    </div>
+                    <div class="square">
+                        <img src="../../public/img/people/PHCF7465.JPG" alt="" />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -17,7 +69,54 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/app.scss' as *;
 
+/* Reset CSS */
 
+img {
+	max-width: 100%;
+	height: auto;
+	vertical-align: middle;
+	display: inline-block;
+}
+
+/* Main CSS */
+
+.grid-wrapper > div > img {
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	border-radius: 2px;
+}
+
+.grid-wrapper {
+	display: grid;
+	grid-gap: 10px;
+	grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+	grid-auto-rows: 200px;
+	grid-auto-flow: dense;
+}
+.grid-wrapper .wide {
+	grid-column: span 2;
+}
+.grid-wrapper .tall {
+	grid-row: span 2;
+    transition: 1s;
+}
+.grid-wrapper .big {
+    grid-column: span 2;
+    grid-row: span 2;
+}
+
+// HOVER FILTRO COLORE, INGRADIMENTO FOTO, BOX SHADOW
+
+.grid-wrapper .square, .tall, .wide, .big{
+    transition: 1s;
+}
+
+.grid-wrapper .tall:hover, .big:hover, .wide:hover, .square:hover {
+    // filter: grayscale(100%);
+    scale: 2;
+    box-shadow: 0 0 6px 3px rgb(239, 239, 211);
+}
 
  
 
