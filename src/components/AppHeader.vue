83b744b100f060b2d9650bf952b68415d1e30364
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-12-13 17:45:19 +0100

    update navbar with hamburger menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,6 +3,7 @@ export default{
     name: 'AppHeader',
     data() {
         return {
+            isMenuOpen: false,
             menuItems: [
                 {
                     label: 'Home',
@@ -49,6 +50,25 @@ export default{
             ]
         }
     },
+
+    // Funzione per far chiudere il menu UL senza premere sulla X, schiacciando anche sullo schermo
+
+    mounted() {
+        document.addEventListener('click', this.handleClickOutside);
+    },
+    beforeDestroy() {
+        document.removeEventListener('click', this.handleClickOutside);
+    },
+    methods: {
+        handleClickOutside(event) {
+            const menuToggle = this.$el.querySelector('#menu-toggle');
+
+        // Check if the click is outside of the menu and toggle button
+            if (menuToggle && !menuToggle.contains(event.target)) {
+                this.isMenuOpen = false;
+            }
+        },
+    },
 }
 </script>
 
@@ -61,9 +81,35 @@ export default{
                             <h1>Wander with Kri</h1>
                         </router-link>
                     </div>
-                    <div class="col d-flex justify-content-lg-end justify-content-center">
-                        <nav>
+                    
+                    <!-- NAVBAR COMPLETA -->
+
+                    <div class="col d-flex justify-content-end d-md-block d-none">
+                        <nav class="d-flex justify-content-end">
                             <ul>
+                                <li v-for="(item, index) in menuItems" :key="index">
+                                    <router-link :to="{name: item.routeName}" class="list-item">
+                                        {{ item.label }}
+                                    </router-link>
+                                </li>
+                                <li class="d-none" v-for="(item, index) in menuCategory" :key="index">
+                                    <router-link :to="{name: item.routeName}" class="list-item">
+                                        {{ item.label }}
+                                    </router-link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <!-- NAVBAR HAMBURGER -->
+
+                    <div class="col d-flex justify-content-end d-md-none d-block">
+                        <nav class="d-flex justify-content-end">
+                            <input id="menu-toggle" type="checkbox" v-model="isMenuOpen" />
+                            <label class='menu-button-container' for="menu-toggle">
+                                <div class='menu-button'></div>
+                            </label>
+                            <ul class="menu" v-show="isMenuOpen">
                                 <li v-for="(item, index) in menuItems" :key="index">
                                     <router-link :to="{name: item.routeName}" class="list-item">
                                         {{ item.label }}
@@ -123,6 +169,114 @@ export default{
                     .logo{
                         display: flex;
                         justify-content: center;
+                    }
+                }
+
+                // HAMBURGER
+
+                .menu {
+                    display: flex;
+                    flex-direction: row;
+                    list-style-type: none;
+                    margin: 0;
+                    padding: 0;
+                    z-index: 9999;
+                }
+
+                .menu > li {
+                    overflow: hidden;
+                }
+
+                .menu-button-container {
+                    display: none;
+                    height: 100%;
+                    width: 30px;
+                    cursor: pointer;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                }
+
+                #menu-toggle {
+                    display: none;
+                }
+
+                // TOGGLE HAMBURGER
+
+                .menu-button,
+                .menu-button::before,
+                .menu-button::after {
+                    display: block;
+                    background-color: black;
+                    position: absolute;
+                    height: 4px;
+                    width: 30px;
+                    transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+                    border-radius: 2px;
+                }
+
+                .menu-button::before {
+                content: '';
+                    margin-top: -8px;
+                }
+
+                .menu-button::after {
+                content: '';
+                    margin-top: 8px;
+                }
+
+                #menu-toggle:checked + .menu-button-container .menu-button::before {
+                    margin-top: 0px;
+                    transform: rotate(405deg);
+                }
+
+                #menu-toggle:checked + .menu-button-container .menu-button {
+                    background: rgba(255, 255, 255, 0);
+                }
+
+                #menu-toggle:checked + .menu-button-container .menu-button::after {
+                    margin-top: 0px;
+                    transform: rotate(-405deg);
+                }
+
+                @media (max-width: 768px) {
+                    .menu-button-container {
+                        display: flex;
+                    }
+                    .menu {
+                        position: absolute;
+                        top: 12%;
+                        margin-top: 50px;
+                        left: 0;
+                        flex-direction: column;
+                        width: 100%;
+                        justify-content: center;
+                        align-items: center;
+                    }
+                    #menu-toggle ~ .menu li {
+                        height: 0;
+                        margin: 0;
+                        padding: 0;
+                        border: 0;
+                        transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+                    }
+                    #menu-toggle:checked ~ .menu li {
+                        border-top: 1px solid #333;
+                        height: 2.5em;
+                        padding: 0.5em;
+                        transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+                    }
+                    .menu > li {
+                        display: flex;
+                        justify-content: center;
+                        margin: 0;
+                        padding: 0.5em 0;
+                        width: 100%;
+                        color: white;
+                        background-color: rgba(255, 255, 255, 0.584);
+                    }
+                    .menu > li:not(:last-child) {
+                        border-bottom: 1px solid #444;
                     }
                 }
             }
