54c63f62515a5405cb65f46be431dfd1eb1f597a
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-12-19 19:24:32 +0100

    update router.js

--- src/router.js
+++ src/router.js
@@ -20,7 +20,7 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
-            path: '/homepage',
+            path: '/',
             name: 'home',
             component: Home
         },
